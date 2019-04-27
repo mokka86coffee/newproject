afa20dff52b2b753f33507c250337d490732f5e5
mokka86coffee <mokka86coffee@gmail.com>
2019-04-27 22:29:02 +0300

    added experimentalDecorators for TypeScript

--- src/index.js
+++ src/index.js
@@ -3,19 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
+import DevTools from 'mobx-react-devtools';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 
-class App extends React.Component {
+@observer class Counter extends React.Component {
+
+    @observable count = 0;
+    
+    handleCount = () => {}
+
     render() {
         return (
             <div className="app">
-                <h1>Hello mobx</h1>
+                <DevTools />
+                <h1>Mobx счетчик</h1>
+                <h2>{ this.count }</h2>
+                <button>-1</button>
+                <button>+1</button>
             </div>
         )
     }
 }
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Counter />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
